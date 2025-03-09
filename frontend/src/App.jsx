d3f627d8244cfc5e8aef56513fd8e67bf1764518
okan <okan28001@gmail.com>
2025-03-09 12:50:06 +0300

    Build topbar layout as responsive inside of the header componend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import UserLayout from "./components/Layout/UserLayout";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route>{/*User Layout */}</Route>
+        <Route path="/" element={<UserLayout />}>
+          {/*User Layout */}
+        </Route>
         <Route>{/*Admin Layout */}</Route>
       </Routes>
     </BrowserRouter>
